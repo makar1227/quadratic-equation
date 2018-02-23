93bb43f369107ab79f46ab6bf0f2fa061ec04ce4
makar1227 <makar1227@gmail.com>
2018-02-23 23:38:07 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
-module.exports = function solveEquation(equation) {
-  // your implementation
+module.exports =
+function solveEquation(equation) {
+    let arr = equation.split(" ");
+
+    let a = [];
+    a.push(arr[7]);
+    a.push(arr[8]);
+    let b = a.join("");
+    let d = Math.sqrt(parseInt(arr[4]) * parseInt(arr[4]) - 4 * parseInt(arr[0]) * parseInt(b));
+
+    a.length=0;
+    a.push(arr[3]);
+    a.push(arr[4]);
+    let c = parseInt(a.join(""));
+    let a1 =Math.round( (-(c) + d) / (2 * parseInt(arr[0])));
+    let a2 =Math.round( (-(c) - d) / (2 * parseInt(arr[0])));
+    a.length=0;
+    a.push(a1);
+    a.push(a2);
+    return a.sort((x,y)=>x-y);
 }
